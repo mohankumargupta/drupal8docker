ecd54a48960623e4fb5429ba294a25b36faefaca
Mohan Gupta <mohangupta@live.com>
2014-10-28 18:28:12 +1100

    angulars example module

---
+++ drushmake.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#/usr/bin/supervisord
+apache2ctl start
+service mysql start
+cd /var/www
+drush dl drupalmoduleupgrader-8.x-0.6
+drush en -y drupalmoduleupgrader
+cd /var/www
+COMPOSER_BIN_DIR=bin composer require --dev drupal/console:@stable
+cd /var/www/modules 
+git clone https://github.com/alexrayu/angulars
+drush en  -y angulars
+exit 0
