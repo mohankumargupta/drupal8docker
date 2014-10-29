3f4080c40071bb6ea53b197f3c57ce78ef586d0d
Mohan Gupta <mohangupta@live.com>
2014-10-30 10:51:43 +1100

    runcontainer now automatically mounts and ssh into container

--- runcontainer.sh
+++ runcontainer.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-docker run -d -p 8080:80 -p 9000:9000 -p 7224:22 -v /home/mohan/docker/mohandrupal8/mohan:/var/www/modules/mohan mohangupta/mohangupta8
+set -x
+if [ `mount|grep drupal8devel|wc -l` -ne 0  ]
+then
+/home/mohan/detachdrupal8.sh
+fi
+
+if [ `docker ps -a|wc -l` -ne "1"   ]
+then
+docker rm -f $(docker ps -a -q)
+fi
+docker run -d -p 8080:80 -p 9000:9000 -p 7224:22 -v /home/mohan/PhpstormProjects/mm/modules/mohan:/var/www/modules/mohan -v /home/mohan/PhpstormProjects/mm/themes/angularmohan:/var/www/themes/angularmohan   mohangupta/mohangupta8
+sleep 5
+/home/mohan/attachdrupal8.sh
+ssh -p 7224 root@localhost
